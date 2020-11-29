5b0993ef062232544f60c94887edd43d11349667
Vita <vita.gladka@gmail.com>
2020-11-29 12:08:40 +0200

    added str

---
+++ src/js/refs.js
@@ -0,0 +1,10 @@
+export default{
+    header: document.querySelector(".header"),
+    nav: document.querySelector(".nav"),
+    ul: document.querySelector(".navList"),
+
+    homeSection: document.querySelector('.home'),
+    aboutSection: document.querySelector('.about'),
+    contactsSection: document.querySelector('.contacts'),
+
+}
