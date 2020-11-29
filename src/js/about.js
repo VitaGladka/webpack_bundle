80019306e6b3b8386cdd5407c0c286afee3c487c
Vita <vita.gladka@gmail.com>
2020-11-29 13:45:50 +0200

    added sections

--- src/js/about.js
+++ src/js/about.js
@@ -1,1 +1,47 @@
 import '../css/about.css'
+import myAutoPark from '../data/items.js'
+// console.log(myAutoPark);
+import aboutRefs from './refs.js'
+const {aboutSection} = aboutRefs;
+// console.log(aboutSection);
+
+function createCarsList (arr) {
+    // console.log(arr)
+    return arr.map((car)=>{
+        //
+        const {name, picture, price} = car;
+        // console.log(name)
+        // console.log(picture)
+        const imgName = document.createElement('h3');
+        imgName.textContent = name;
+        const imageWrapper = document.createElement('div');
+
+        const pictures = picture.map((pic)=>{
+            console.log(pic);
+            const img = document.createElement('img')
+            img.setAttribute('src', pic)
+            img.setAttribute('alt', name)
+            img.setAttribute('width', '200px')
+            // console.log(img);
+            return img
+        })
+        imageWrapper.append(...pictures);
+        // console.log(imageWrapper)
+        // console.log(price)
+
+        const carPrice = document.createElement('p');
+        carPrice.textContent = price;
+        const item = document.createElement('li');
+        item.append(imgName, imageWrapper, carPrice)
+        return item;
+
+    })
+}
+const carsItems = createCarsList(myAutoPark);
+console.log(carsItems);
+
+const carsList = document.createElement('ul');
+carsList.append(...carsItems);
+// console.log(carsList);
+
+aboutSection.append(carsList);
--- src/js/home.js
+++ src/js/home.js
@@ -1,1 +1,19 @@
 import '../css/home.css'
+import refs from './refs.js'
+console.log(refs);
+console.log(refs.homeSection);
+// деструктуразация  обьекта доступа (вытаскиваю свойства)
+
+const {homeSection} = refs;
+console.log(homeSection);
+
+const homeTitle = `<h2 class="homeTitle">Home</h2>`
+
+homeSection.insertAdjacentHTML('afterbegin', homeTitle);
+
+const homeContent = document.createElement('p');
+const content = `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquid, ducimus!`;
+homeContent.textContent = content;
+console.log(homeContent);
+const title = document.querySelector('.homeTitle');
+title.appendChild(homeContent);
