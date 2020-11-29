8b8034a55bd3b0d7a173c0e057418185bb4b9c43
Vita <vita.gladka@gmail.com>
2020-11-29 18:45:37 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import css from "./css/styles.css";
-import './js/home.js';
-import './js/about.js';
+// import './js/home.js';
+// import './js/about.js';
 import './js/contacts.js';
 
--- src/js/contacts.js
+++ src/js/contacts.js
@@ -1,1 +1,43 @@
-import '../css/contacts.css'
+import "../css/contacts.css";
+import references from "./refs.js";
+const { contactsSection } = references;
+
+const game = ["камень", "ножницы", "бумага"];
+function createButtons(arr) {
+  return arr.map((el) => {
+    const button = document.createElement("button");
+    button.textContent = el;
+    button.dataset.key = el;
+
+    return button;
+  });
+}
+const buttons = createButtons(game);
+// console.log(buttons);
+contactsSection.append(...buttons);
+
+const result = document.createElement("p");
+contactsSection.append(result);
+
+contactsSection.addEventListener("click", (evt) => {
+  let index = Math.round(Math.random() * (game.length - 1));
+  console.log(index);
+  let user = evt.target.dataset.key;
+  //   console.log(user);
+  let computer = game[index];
+  //   console.log(computer);
+  console.log(`user: ${user} - computer: ${computer}`);
+
+  let msg;
+  if (user === computer) {
+    msg = "ничья";
+  } else {
+    (user === "ножницы" && computer === "бумага") ||
+    (user === "бумага" && computer === "камень") ||
+    (user === "камень" && computer === "ножницы")
+      ? (msg = "User выиграл")
+      : (msg = "Computer выиграл");
+  }
+
+  result.textContent = msg;
+});
